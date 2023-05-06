9ab019a51484748eea9a82da61566084e89d5553
pranjal.more_comp21 <pranjal.more_comp21@pccoer.in>
2023-05-06 18:10:12 +0530

    login register

--- frontend/src/components/loginForm.js
+++ frontend/src/components/loginForm.js
@@ -1,62 +1,67 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import { useState } from "react";
+
+import axios from "axios";
 import { Link } from "react-router-dom";
 
 function LoginForm() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-
-  const [error, setError] = useState('');
-
+  const [error, setError] = useState("");
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     try {
-      const response = await axios.post('http://localhost:5000/login', {
+      const response = await axios.post("http://localhost:5000/login", {
         email,
         password,
       });
 
-
-
       // If authentication was successful, store the user object in local storage and redirect the user to the home page
-      localStorage.setItem('user', JSON.stringify(response.data));
-      window.location.href = '/';
-
+      localStorage.setItem("user", JSON.stringify(response.data));
+      window.location.href = "/";
     } catch (error) {
       console.error(error);
-      setError('Invalid email or password. Please try again.');
+      setError("Invalid email or password. Please try again.");
     }
   };
- 
+
   return (
-
-     <div className='g1'>
-         <div style={{ width: '100%', height: '100px', backgroundColor: 'transparent' }}></div>
-      <h1>Login Form</h1>
-      {error && <p>{error}</p>}
-      <form onSubmit={handleSubmit}>
-
-        <input
-          className='linput'
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-        />
-        <input
-          className='linput'
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-        />
-        <button className='lbtn' type="submit">Log In</button>
-      </form>
-      <p className='rswitch '>Don't have an account? <Link to="/register">Register here</Link>.</p>
+    <div className="g1">
+      <div className="login_form_container">
+        <div className="left">
+          {error && <p>{error}</p>}
+          <form onSubmit={handleSubmit} className="form_container">
+            <h1>Login to Your Account</h1>
+            <input
+              className="linput"
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
+            <input
+              className="linput"
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+            <button className="lbtn" type="submit">
+              Log In
+            </button>
+          </form>
+        </div>
+        <div className="right">
+          <h1>New Here ?</h1>
+          <Link to="/register">
+            <button type="button" className="white_btn">
+              Sing Up
+            </button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
